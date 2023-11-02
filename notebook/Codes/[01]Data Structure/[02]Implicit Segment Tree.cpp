35c31356f9a08c85de3021280c5505e73b181e1b
Mushfiq Talha <58101262+Talha76@users.noreply.github.com>
2023-11-02 20:43:14 +0600

    Update [02]Implicit Segment Tree.cpp

--- notebook/Codes/[01]Data Structure/[02]Implicit Segment Tree.cpp
+++ notebook/Codes/[01]Data Structure/[02]Implicit Segment Tree.cpp
@@ -1,35 +1,46 @@
 struct node {
-  int val;
-  node *lft, *rt;
-  node() {}
-  node(int val = 0) : val(val), lft(NULL), rt(NULL) {}
+    ll val;
+    ll lazy;
+    int left, right;
 };
-
-struct implicit_segtree {
-  node *root;
-  implicit_segtree() {}
-  implicit_segtree(int n) {
-    root = new node(n);
-  }
-  void update(node *now, int L, int R, int idx, int val) {
-    if (L == R) {
-      now -> val += val;
-      return;
+node nodes[4*1000006];
+int sz;
+void reset(int nd) {
+    nodes[nd].left = nodes[nd].right = -1;
+    nodes[nd].val = nodes[nd].lazy = 0;
+}
+int getLeft(int nd) {
+    if(nodes[nd].left == -1) {
+        nodes[nd].left = sz++;
+        reset(nodes[nd].left);
     }
-    int mid = L + (R - L) / 2;
-    if (now->lft == NULL) now->lft = new node(mid - L + 1);
-    if (now->rt == NULL) now->rt = new node(R - mid);
-    if (idx <= mid) update(now->lft, L, mid, idx, val);
-    else update(now->rt, mid + 1, R, idx, val);
-    now->val = (now->lft)->val + (now->rt)->val;
-  }
-
-  int query(node *now, int L, int R, int k) {
-    if (L == R) return L;
-    int mid = L + (R - L) / 2;
-    if (now->lft == NULL) now->lft = new node(mid - L + 1);
-    if (now->rt == NULL) now->rt = new node(R - mid);
-    if (k <= (now->lft)->val) return query(now->lft, L, mid, k);
-    else return query(now->rt, mid + 1, R, k - (now->lft)->val);
-  }
-};
+    return nodes[nd].left;
+}
+int getRight(int nd) {
+    if(nodes[nd].right == -1) {
+        nodes[nd].right = sz++;
+        reset(nodes[nd].right);
+    }
+    return nodes[nd].right;
+}
+void update(int nd, int s, int e, int l, int r, ll val) {
+    if(e < l or s > r) return;
+    if(s >= l and e <= r) {
+        nodes[nd].val += val*(e-s + 1);
+        nodes[nd].lazy += val;
+        return;
+    }
+    int m = (s + e) / 2;
+    update(getLeft(nd), s, m, l ,r, val);
+    update(getRight(nd), m+1, e, l, r, val);
+    nodes[nd].val = nodes[nodes[nd].left].val + nodes[nodes[nd].right].val + nodes[nd].lazy * (e-s+1);
+}
+ll query(int nd, int s, int e, int l, int r, ll c) {
+    if(s > r or e < l) return 0;
+    if(s >= l and e <= r) {
+        return nodes[nd].val + c*(e-s+1);
+    }
+    int m = (s+e)/2;
+    c += nodes[nd].lazy;
+    return query(getLeft(nd), s, m, l, r, c) + query(getRight(nd), m+1, e, l, r, c);
+}
