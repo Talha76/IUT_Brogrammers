b18fd54e096af1d6dff35c64e1b490afb9bfd6d7
Mushfiq Talha <58101262+Talha76@users.noreply.github.com>
2023-11-02 21:06:17 +0600

    Update [10]Dinic Max Flow.cpp

--- notebook/Codes/[04]Graph/[10]Dinic Max Flow.cpp
+++ notebook/Codes/[04]Graph/[10]Dinic Max Flow.cpp
@@ -1,63 +1,99 @@
-/// flow with demand(lower bound) only for DAG
-// create new src and sink
-// add_edge(new src, u, sum(in_demand[u]))
-// add_edge(u, new sink, sum(out_demand[u]))
-// add_edge(old sink, old src, inf)
-//  if (sum of lower bound == flow) then demand satisfied
-// flow in every edge i = demand[i] + e.flow
+/**
+Implementation of Dinic's algorithm with optional scaling
+Source: Chilli (https://codeforces.com/blog/entry/66006)
 
-using Ti = long long;
-const Ti INF = 1LL << 60;
-struct edge {
-  int v, u;
-  Ti cap, flow = 0;
-  edge(int v, int u, Ti cap) : v(v), u(u), cap(cap) {}
-};
-const int N = 1e5 + 50;
-vector<edge> edges;
-vector<int> adj[N];
-int m = 0, n;
-int level[N], ptr[N];
-queue<int> q;
-bool bfs(int s, int t) {
-  for (q.push(s), level[s] = 0; !q.empty(); q.pop()) {
-    for (int id : adj[q.front()]) {
-      auto &ed = edges[id];
-      if (ed.cap - ed.flow > 0 and level[ed.u] == -1)
-        level[ed.u] = level[ed.v] + 1, q.push(ed.u);
+Complexity: O(ans*E) or O(V^2E) without scaling, O(VE log(U)) with scaling,
+Scaling performs much better in worst case, but has much higher constant factor
+To enable scaling, call maxFlow(true)
+Everything 0-indexed
+*/
+namespace Dinic {
+    typedef long long LL;
+    const int N = 5005, K = 60; /// N > no of nodes, K >= max bits in capacity
+    const LL INF = 1e18;
+    struct Edge { int frm, to; LL cap, flow; };
+    int s, t, n;
+    int level[N], ptr[N];
+    vector<Edge> edges;
+    vector<int> adj[N];
+    void init(int nodes) {
+        n = nodes;
+        for (int i=0; i<n; i++)    adj[i].clear();
+        edges.clear();
     }
-  }
-  return level[t] != -1;
-}
-Ti dfs(int v, Ti pushed, int t) {
-  if (pushed == 0) return 0;
-  if (v == t) return pushed;
-  for (int &cid = ptr[v]; cid < adj[v].size(); cid++) {
-    int id = adj[v][cid];
-    auto &ed = edges[id];
-    if (level[v] + 1 != level[ed.u] || ed.cap - ed.flow < 1) continue;
-    Ti tr = dfs(ed.u, min(pushed, ed.cap - ed.flow), t);
-    if (tr == 0) continue;
-    ed.flow += tr;
-    edges[id ^ 1].flow -= tr;
-    return tr;
-  }
-  return 0;
-}
-void init(int nodes) {
-  m = 0, n = nodes;
-  for (int i = 0; i < n; i++) level[i] = -1, ptr[i] = 0, adj[i].clear();
-}
-void addEdge(int v, int u, Ti cap) {
-  edges.emplace_back(v, u, cap), adj[v].push_back(m++);
-  edges.emplace_back(u, v, 0), adj[u].push_back(m++);
-}
-Ti maxFlow(int s, int t) {
-  Ti f = 0;
-  for (auto &ed : edges) ed.flow = 0;
-  for (; bfs(s, t); memset(level, -1, n * 4)) {
-    for (memset(ptr, 0, n * 4); Ti pushed = dfs(s, INF, t); f += pushed)
-      ;
-  }
-  return f;
+    /// For adding undirected Edge (u, v, c) call addEdge(u, v, c, c);
+    int addEdge(int a, int b, LL cap, LL revcap = 0) {
+        edges.push_back({a, b, cap, 0});
+        edges.push_back({b, a, revcap, 0});
+        adj[a].push_back(edges.size()-2);
+        adj[b].push_back(edges.size()-1);
+        return edges.size()-2;
+    }
+    bool bfs(LL lim) {
+        fill(level, level+n, -1);
+        level[s] = 0;
+        queue<int> q;
+        q.push(s);
+
+        while (!q.empty() && level[t] == -1) {
+            int v = q.front();
+            q.pop();
+            for (int id: adj[v]) {
+                Edge e = edges[id];
+                if (level[e.to] == -1 && e.cap - e.flow >= lim) {
+                    q.push(e.to);
+                    level[e.to] = level[v] + 1;
+                }
+            }
+        }
+        return level[t] != -1;
+    }
+    LL dfs(int v, LL flow) {
+        if (v == t || !flow)        return flow;
+        for (; ptr[v] < adj[v].size(); ptr[v]++) {
+            int eid = adj[v][ptr[v]];
+            Edge &e = edges[eid];
+            if (level[e.to] != level[v] + 1)    continue;
+            if (LL pushed = dfs(e.to, min(flow, e.cap - e.flow))) {
+                e.flow += pushed;
+                edges[eid^1].flow -= pushed;
+                return pushed;
+            }
+        }
+        return 0;
+    }
+    LL maxFlow(int source, int sink, bool SCALING = false) {
+        s = source, t = sink;
+
+        long long flow = 0;
+        for (LL lim = SCALING ? (1LL << K) : 1; lim > 0; lim >>= 1) {
+            while (bfs(lim)) {
+                fill(ptr, ptr+n, 0);
+                while (LL pushed = dfs(s, INF)) flow += pushed;
+            }
+        }
+        return flow;
+    }
+    bool leftOfMinCut(int x) {return level[x] != -1;}
+    /// Only works for undirected graph, Make sure to add UNDIRECTED edges. (u, v, c, c)
+    /// returns n by n matrix flow, st flow[i][j] = maxFlow
+    /// tree holds the edges of a gomory-hu tree of the graph
+    vector<vector<LL>> allPairMaxFlow(vector<Edge> &tree) {
+        tree.clear();
+        vector<vector<LL>> flow(n, vector<LL> (n, INF));
+        vector<int> par(n);
+        for (int i=1; i<n; i++) {
+            for (auto &e: edges)    e.flow = 0;
+            LL f = maxFlow(i, par[i]);
+            tree.push_back({i, par[i], f});
+
+            for (int j=i+1; j<n; j++)
+                if (par[j] == par[i] && leftOfMinCut(j)) par[j] = i;
+
+            flow[i][par[i]] = flow[par[i]][i] = f;
+            for (int j=0; j<i; j++)
+                if (j != par[i])    flow[i][j] = flow[j][i] = min(f, flow[par[i]][j]);
+        }
+        return flow;
+    }
 }
