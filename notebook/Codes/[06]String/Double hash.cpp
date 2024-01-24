29c051175ffaf539339a573f4a4d0d7b60a83e48
Adib Farhan <98574339+Brownbear2710@users.noreply.github.com>
2024-01-24 21:26:25 +0600

    Update Double hash.cpp

--- notebook/Codes/[06]String/Double hash.cpp
+++ notebook/Codes/[06]String/Double hash.cpp
@@ -1,76 +1,100 @@
 // define +, -, * for (PLL, LL) and (PLL, PLL), % for (PLL, PLL);
 PLL base(1949313259, 1997293877);
-PLL mod(2091573227, 2117566807);
-
-PLL power(PLL a, LL p) {
-  PLL ans = PLL(1, 1);
-  for(; p; p >>= 1, a = a * a % mod) {
-      if(p & 1) ans = ans * a % mod;
-  }
-  return ans;
+namespace Hashing {
+    using LL = long long;
+    using PLL = pair<LL,LL>;
+    #define ff first
+    #define ss second
+    const PLL M = {2091573227, 2117566807};        ///Should be large primes
+    const LL base = 1259;                ///Should be larger than alphabet size
+    const int N = 1e6+7;                 ///Highest length of string
+    PLL operator+ (const PLL& a, LL x)     {return {a.ff + x, a.ss + x};}
+    PLL operator- (const PLL& a, LL x)     {return {a.ff - x, a.ss - x};}
+    PLL operator* (const PLL& a, LL x)     {return {a.ff * x, a.ss * x};}
+    PLL operator+ (const PLL& a, PLL x)    {return {a.ff + x.ff, a.ss + x.ss};}
+    PLL operator- (const PLL& a, PLL x)    {return {a.ff - x.ff, a.ss - x.ss};}
+    PLL operator* (const PLL& a, PLL x)    {return {a.ff * x.ff, a.ss * x.ss};}
+    PLL operator% (const PLL& a, PLL m)    {return {a.ff % m.ff, a.ss % m.ss};}
+    ostream& operator<<(ostream& os, PLL hash) {
+        return os<<"("<<hash.ff<<", "<<hash.ss<<")";
+    }
+    PLL pb[N];      ///powers of base mod M
+    ///Call pre before everything
+    void hashPre() {
+        pb[0] = {1,1};
+        for (int i=1; i<N; i++)     pb[i] = (pb[i-1] * base)%M;
+    }
+    ///Calculates hashes of all prefixes of s including empty prefix
+    vector<PLL> hashList(string s) {
+        int n = s.size();
+        vector<PLL> ans(n+1);
+        ans[0] = {0,0};
+        for (int i=1; i<=n; i++)    ans[i] = (ans[i-1] * base + s[i-1])%M;
+        return ans;
+    }
+    ///Calculates hash of substring s[l..r] (1 indexed)
+    PLL substringHash(const vector<PLL> &hashlist, int l, int r) {
+        return (hashlist[r]+(M-hashlist[l-1])*pb[r-l+1])%M;
+    }
+    ///Calculates Hash of a string
+    PLL Hash (string s) {
+        PLL ans = {0,0};
+        for (int i=0; i<s.size(); i++)  ans=(ans*base + s[i])%M;
+        return ans;
+    }
+    ///Tested on https://toph.co/p/palindromist
+    ///appends c to string
+    PLL append(PLL cur, char c) {
+        return (cur*base + c)%M;
+    }
+    ///Tested on https://toph.co/p/palindromist
+    ///prepends c to string with size k
+    PLL prepend(PLL cur, int k, char c) {
+        return (pb[k]*c + cur)%M;
+    }
+    ///Tested on https://toph.co/p/chikongunia
+    ///replaces the i-th (0-indexed) character from right from a to b;
+    PLL replace(PLL cur, int i, char a, char b) {
+        return cur + pb[i] * (M+b-a)%M;
+    }
+    ///Erases c from front of the string with size len
+    PLL pop_front(PLL hash, int len, char c) {
+        return (hash + pb[len-1]*(M-c))%M;
+    }
+    ///Tested on https://toph.co/p/palindromist
+    ///concatenates two strings where length of the right is k
+    PLL concat(PLL left, PLL right, int k) {
+        return (left*pb[k] + right)%M;
+    }
+    PLL power (const PLL& a, LL p) {
+        if (p==0)   return {1,1};
+        PLL ans = power(a, p/2);
+        ans = (ans * ans)%M;
+        if (p%2)    ans = (ans*a)%M;
+        return ans;
+    }
+    PLL inverse(PLL a)  {
+        if (M.ss == 1)  return power(a, M.ff-2);
+        return power(a, (M.ff-1)*(M.ss-1)-1);
+    }
+    ///Erases c from the back of the string
+    PLL invb = inverse({base, base});
+    PLL pop_back(PLL hash, char c) {
+        return ((hash-c+M)*invb)%M;
+    }
+    ///Tested on https://toph.co/p/palindromist
+    ///Calculates hash of string with size len repeated cnt times
+    ///This is O(log n). For O(1), pre-calculate inverses
+    PLL repeat(PLL hash, int len, LL cnt) {
+        PLL mul = ((pb[len*cnt]-1+M) * inverse(pb[len]-1+M))%M;
+        PLL ans = (hash*mul);
+        if (pb[len].ff == 1)    ans.ff = hash.ff*cnt;
+        if (pb[len].ss == 1)    ans.ss = hash.ss*cnt;
+        return ans%M;
+    }
+    struct pair_hash {
+        inline std::size_t operator()(const std::pair<LL,LL> & v) const {
+            return v.first*31+v.second;
+        }
+    };
 }
-
-PLL inverse(PLL a) { return power(a, (mod.ff - 1) * (mod.ss - 1) - 1); }
-PLL inv_base = inverse(base);
-PLL val;
-vector<PLL> P;
-
-void hash_init(int n) {
-  P.resize(n + 1);
-  P[0] = PLL(1, 1);
-  for (int i = 1; i <= n; i++) P[i] = (P[i - 1] * base) % mod;
-}
-PLL append(PLL cur, char c) { return (cur * base + c) % mod; }
-/// prepends c to string with size k
-PLL prepend(PLL cur, int k, char c) { return (P[k] * c + cur) % mod; }
-/// replaces the i-th (0-indexed) character from right from a to b;
-PLL replace(PLL cur, int i, char a, char b) {
-  cur = (cur + P[i] * (b - a)) % mod;
-  return (cur + mod) % mod;
-}
-/// Erases c from the back of the string
-PLL pop_back(PLL hash, char c) {
-  return (((hash - c) * inv_base) % mod + mod) % mod;
-}
-/// Erases c from front of the string with size len
-PLL pop_front(PLL hash, int len, char c) {
-  return ((hash - P[len - 1] * c) % mod + mod) % mod;
-}
-/// concatenates two strings where length of the right is k
-PLL concat(PLL left, PLL right, int k) { return (left * P[k] + right) % mod; }
-/// Calculates hash of string with size len repeated cnt times
-/// This is O(log n). For O(1), pre-calculate inverses
-PLL repeat(PLL hash, int len, LL cnt) {
-  PLL mul = (P[len * cnt] - 1) * inverse(P[len] - 1);
-  mul = (mul % mod + mod) % mod;
-  PLL ret = (hash * mul) % mod;
-  if (P[len].ff == 1) ret.ff = hash.ff * cnt;
-  if (P[len].ss == 1) ret.ss = hash.ss * cnt;
-  return ret;
-}
-LL get(PLL hash) { return ((hash.ff << 32) ^ hash.ss); }
-struct hashlist {
-  int len;
-  vector<PLL> H, R;
-  hashlist() {}
-  hashlist(string& s) {
-    len = (int)s.size();
-    hash_init(len);
-    H.resize(len + 1, PLL(0, 0)), R.resize(len + 2, PLL(0, 0));
-    for (int i = 1; i <= len; i++) H[i] = append(H[i - 1], s[i - 1]);
-    for (int i = len; i >= 1; i--) R[i] = append(R[i + 1], s[i - 1]);
-  }
-  /// 1-indexed
-  PLL range_hash(int l, int r) {
-    return ((H[r] - H[l - 1] * P[r - l + 1]) % mod + mod) % mod;
-  }
-  PLL reverse_hash(int l, int r) {
-    return ((R[l] - R[r + 1] * P[r - l + 1]) % mod + mod) % mod;
-  }
-  PLL concat_range_hash(int l1, int r1, int l2, int r2) {
-    return concat(range_hash(l1, r1), range_hash(l2, r2), r2 - l2 + 1);
-  }
-  PLL concat_reverse_hash(int l1, int r1, int l2, int r2) {
-    return concat(reverse_hash(l2, r2), reverse_hash(l1, r1), r1 - l1 + 1);
-  }
-};
